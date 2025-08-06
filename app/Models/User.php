0c89dd9738a56bce3b14d7e36ba529d98f3efe0e
adik <i.adilets@gmail.com>
2025-08-07 01:24:08 +0600

    adding access

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -12,7 +14,7 @@ use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasRoles;
 
@@ -70,5 +72,9 @@ class User extends Authenticatable
 
     public function client(): BelongsTo {
         return $this->belongsTo(Client::class);
+    }
+
+    public function canAccessPanel(Panel $panel): bool {
+        return true;
     }
 }
